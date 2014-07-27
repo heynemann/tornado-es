ea39c95e63c88224e024ff1b2da528de1e13fad3
Bernardo Heynemann <heynemann@gmail.com>
2014-07-27 16:08:36 -0300

    Tornado greater than 2.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ A tornado-powered python library that provides asynchronous access to elasticsea
       include_package_data=True,
       zip_safe=True,
       install_requires=[
-          'tornado==2.4',
+          'tornado>=2.4',
           'six==1.7.3',
       ],
       tests_require=[
